8c29f51b6c789c549aa9d49d2655026d15d0be95
MatteoNeri99 <matteolavoro99@gmail.com>
2024-07-22 08:49:40 +0200

    aggiunti 20 treni alla tabella Train tramite seeder con dati Fake tramite Faker

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,16 +2,34 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
+        for ($i=0; $i <20 ; $i++) {
+            $train= new Train();
+            $train->Azienda=$faker->sentence(7);
+            $train->Stazione_di_partenza=$faker->sentence(7);
+            $train->Stazione_di_arrivo=$faker->sentence(7);
+            $train->Orario_di_partenza=$faker->dateTime();
+            $train->Orario_di_arrivo=$faker->dateTime();
+            $train->Codice_treno=$faker->sentence(7);
+            $train->Numero_Carrozze=$faker->randomNumber(1);
+            $train->In_orario=$faker->boolean();
+            $train->Cancellato=$faker->boolean();
+            $train->save();
+
+
+        }
+
 
     }
 }
